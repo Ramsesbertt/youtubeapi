957f39576ddfc88cb3231b946160de6443451393
Ramsesbertt <156328744+Ramsesbertt@users.noreply.github.com>
2024-10-10 23:44:22 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
-      home: const HomePage(), // Muestra directamente HomePage
+      home: const HomePage(),
       debugShowCheckedModeBanner: false,
     );
   }
