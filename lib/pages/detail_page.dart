2725b861c43798f8aca901a558ff71547c0349dd
Ramsesbertt <156328744+Ramsesbertt@users.noreply.github.com>
2024-10-10 23:44:00 -0500

    Update detail_page.dart

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -25,9 +25,9 @@ class DetailPage extends StatelessWidget {
             const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
-                // Redirigir a YouTube para ver el video
+
                 final url = 'https://www.youtube.com/watch?v=$videoId';
-                // Implementar un webview o redireccionar al navegador
+
               },
               child: const Text('Ver Video'),
             ),
